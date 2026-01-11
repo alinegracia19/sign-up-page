dbf3302273d6ab4d910380a97789811be5e1af1e
alinegracia19 <251604976+alinegracia19@users.noreply.github.com>
2026-01-11 15:11:50 +0700

    Added password validation to the password input

--- script.js
+++ script.js
@@ -6,9 +6,39 @@ const phoneNumberElement = document.getElementById("phone-number");
 const passwordElement = document.getElementById("password");
 const confirmPasswordElement = document.getElementById("confirm-pass");
 const formContainer = document.getElementById("form-container");
+const passValidation = document.querySelector("ul");
+const list1 = document.getElementById("li1");
+const list2 = document.getElementById("li2");
+const list3 = document.getElementById("li3");
 
 const notif = document.createElement("p");
 notif.classList.add("notif");
+
+let password = "";
+
+
+passwordElement.addEventListener("input", e => {
+    passValidation.style.display = "block";
+    password = e.target.value;
+    console.log(password);
+    if (password.length >= 8) {
+        list1.classList.add("valid");
+    } else {
+        list1.classList.remove("valid");
+    }
+
+    if (password.match(/\d+/g)) {
+        list2.classList.add("valid");
+    } else {
+        list2.classList.remove("valid");
+    }
+
+    if (password.match(/\W/g)) {
+        list3.classList.add("valid");
+    } else {
+        list3.classList.remove("valid");
+    }
+})
 
 // let firstName = "";
 // let lastname = "";

744c40a3b0a07758c4f1f9e76fd7bdbf7f458abb
alinegracia19 <251604976+alinegracia19@users.noreply.github.com>
2026-01-11 13:29:58 +0700

    Selected DOM element for validation in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const createAccountBtn = document.getElementById("sign-up");
+const firstNameElement = document.getElementById("first-name");
+const lastNameElement = document.getElementById("last-name");
+const emailElement = document.getElementById("email");
+const phoneNumberElement = document.getElementById("phone-number");
+const passwordElement = document.getElementById("password");
+const confirmPasswordElement = document.getElementById("confirm-pass");
+
+createAccountBtn.addEventListener("click", () => {
+    console.log("Haii");
+    
+})

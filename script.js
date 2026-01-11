584952b47ab51ccff8295fbb3d5eae4b873e327c
alinegracia19 <251604976+alinegracia19@users.noreply.github.com>
2026-01-11 14:31:13 +0700

    Added validation warnings to the sign-up button

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ const emailElement = document.getElementById("email");
 const phoneNumberElement = document.getElementById("phone-number");
 const passwordElement = document.getElementById("password");
 const confirmPasswordElement = document.getElementById("confirm-pass");
+const formContainer = document.getElementById("form-container");
+
+const notif = document.createElement("p");
+notif.classList.add("notif");
+
+// let firstName = "";
+// let lastname = "";
+// let email = "";
+// let phoneNumber = "";
+// let password = "";
+// let confirmPass = "";
 
 createAccountBtn.addEventListener("click", () => {
-    console.log("Haii");
-    
+    if (!firstNameElement.checkValidity() || !lastNameElement.checkValidity() || !emailElement.checkValidity() || !phoneNumberElement.checkValidity() || !passwordElement.checkValidity() || !confirmPasswordElement.checkValidity()) {
+        notif.textContent = "Please enter the correct data";
+        formContainer.appendChild(notif);
+    }
 })
